5e11431f2fb9496b8880f57b1114d0856fad72e2
1-2Mrunali <104629188+1-2Mrunali@users.noreply.github.com>
2022-05-03 17:46:40 +0530

    initial.commit

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let loadImage = (src, callback) => {
 };
 
 let imagePath = (frameNumber, animation) => {
-  return "images/" + animation + "/" + frameNumber + ".png";
+  return "/images/" + animation + "/" + frameNumber + ".png";
 };
 
 let frames = {
